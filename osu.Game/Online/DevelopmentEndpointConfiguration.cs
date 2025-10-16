12ab26f6837e29b873be1d0f4fa1310fdf25f2a3
clxxped <deyontaandersen@outlook.com>
2025-10-16 21:21:42 +0800

    Update DevelopmentEndpointConfiguration.cs

--- osu.Game/Online/DevelopmentEndpointConfiguration.cs
+++ osu.Game/Online/DevelopmentEndpointConfiguration.cs
@@ -10,7 +10,7 @@ namespace osu.Game.Online
             WebsiteUrl = APIUrl = @"https://osu.clxxped.lol";
             APIClientSecret = @"lm2COgFAHiLPIcMtjxUeUbRItRQd79TkwcEsMNHx";
             APIClientID = "1";
-            SpectatorUrl = $@"{APIUrl}/signalr/spectator";
+            SpectatorUrl = $@"https://osu-spectator.clxxped.lol/signalr/spectator";
             MultiplayerUrl = $@"{APIUrl}/signalr/multiplayer";
             MetadataUrl = $@"{APIUrl}/signalr/metadata";
             BeatmapSubmissionServiceUrl = $@"{APIUrl}/beatmap-submission";
